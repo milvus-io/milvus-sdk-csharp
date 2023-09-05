f9eddbd74f00a71a407bff578f46983f04c09790
Shay Rojansky <roji@roji.org>
2023-09-05 23:43:58 +0200

    Basic changes needed to get tests passing on Milvus 2.3

--- Milvus.Client/MilvusIndexInfo.cs
+++ Milvus.Client/MilvusIndexInfo.cs
@@ -9,11 +9,21 @@ public sealed class MilvusIndexInfo
         string fieldName,
         string indexName,
         long indexId,
+        IndexState state,
+        long indexedRows,
+        long totalRows,
+        long pendingIndexRows,
+        string? indexStateFailReason,
         IReadOnlyDictionary<string, string> @params)
     {
         FieldName = fieldName;
         IndexName = indexName;
         IndexId = indexId;
+        State = state;
+        IndexedRows = indexedRows;
+        TotalRows = totalRows;
+        PendingIndexRows = pendingIndexRows;
+        IndexStateFailReason = indexStateFailReason;
         Params = @params;
     }
 
@@ -31,6 +41,31 @@ public sealed class MilvusIndexInfo
     /// Index id.
     /// </summary>
     public long IndexId { get; }
+
+    /// <summary>
+    /// The state of the index.
+    /// </summary>
+    public IndexState State { get; }
+
+    /// <summary>
+    /// The number of rows indexed by this index;
+    /// </summary>
+    public long IndexedRows { get; }
+
+    /// <summary>
+    /// The total rows in the collection.
+    /// </summary>
+    public long TotalRows { get; }
+
+    /// <summary>
+    /// The number of pending rows in the index.
+    /// </summary>
+    public long PendingIndexRows { get; }
+
+    /// <summary>
+    /// If creation of the index failed, contains the reason for the failure.
+    /// </summary>
+    public string? IndexStateFailReason { get; }
 
     /// <summary>
     /// Contains index_type, metric_type, params.
