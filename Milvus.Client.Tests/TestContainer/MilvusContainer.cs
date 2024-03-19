2ccee694092952ca86c2648e9653863951ea8dc5
Shay Rojansky <roji@roji.org>
2024-03-19 17:20:30 +0100

    Switch to using the public Milvus testcontainer
    Continues #57

--- Milvus.Client.Tests/TestContainer/MilvusContainer.cs
+++
@@ -1,7 +0,0 @@
-using DotNet.Testcontainers.Containers;
-using Microsoft.Extensions.Logging;
-
-namespace Milvus.Client.Tests.TestContainer;
-
-public class MilvusContainer(MilvusConfiguration configuration, ILogger logger)
-    : DockerContainer(configuration, logger);
