f0112be6466eb5d6523da0e45e51f63eb09193ef
g.verdier <g.verdier@criteo.com>
2025-11-07 13:36:29 +0100

    Upgrade Milvus

--- Milvus.Client.Tests/MilvusFixture.cs
+++ Milvus.Client.Tests/MilvusFixture.cs
@@ -8,7 +8,7 @@ public sealed class MilvusTestCollection : ICollectionFixture<MilvusFixture>;
 
 public sealed class MilvusFixture : IAsyncLifetime
 {
-    private const string DefaultMilvusImage = "milvusdb/milvus:v2.4.0-rc.1";
+    private const string DefaultMilvusImage = "milvusdb/milvus:v2.5.6";
 
     private readonly MilvusContainer _container = new MilvusBuilder()
         .WithImage(Environment.GetEnvironmentVariable("MILVUS_IMAGE") ?? DefaultMilvusImage)
