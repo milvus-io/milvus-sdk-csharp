51526d0ae4db79bf0ed00ffd5f286864997da1b2
g.verdier <g.verdier@criteo.com>
2025-11-27 11:34:11 +0100

    Copy testcontainer PR

--- Milvus.Client.Tests/MilvusFixture.cs
+++ Milvus.Client.Tests/MilvusFixture.cs
@@ -1,4 +1,5 @@
-using Grpc.Core;
+using Docker.DotNet.Models;
+using DotNet.Testcontainers.Builders;
 using Testcontainers.Milvus;
 using Xunit;
 
@@ -13,8 +14,22 @@ public sealed class MilvusFixture : IAsyncLifetime
 
     private readonly MilvusContainer _container = new MilvusBuilder()
         .WithImage(Environment.GetEnvironmentVariable("MILVUS_IMAGE") ?? DefaultMilvusImage)
-        .WithEnvironment("DEPLOY_MODE", "STANDALONE")
         .WithEnvironment("QUOTA_AND_LIMITS_FLUSH_RATE_COLLECTION_MAX", "-1")
+        // Remove once https://github.com/testcontainers/testcontainers-dotnet/pull/1569 is available.
+        .WithEnvironment("DEPLOY_MODE", "STANDALONE")
+        // Remove once https://github.com/testcontainers/testcontainers-dotnet/pull/1585 is available.
+        .WithWaitStrategy(Wait.ForUnixContainer().UntilContainerIsHealthy())
+        .WithCreateParameterModifier(parameterModifier =>
+        {
+            parameterModifier.Healthcheck = new HealthcheckConfig
+            {
+                Test = ["CMD-SHELL", $"curl -f http://localhost:{MilvusBuilder.MilvusManagementPort}/healthz"],
+                Interval = TimeSpan.FromSeconds(30),
+                Timeout = TimeSpan.FromSeconds(20),
+                StartPeriod = 90 * 1_000_000_000L,
+                Retries = 3,
+            };
+        })
         .Build();
 
     public string Host => _container.Hostname;
@@ -28,37 +43,6 @@ public sealed class MilvusFixture : IAsyncLifetime
     public MilvusClient CreateClient(string database)
         => new(Host, Username, Password, Port, ssl: false, database);
 
-    public async Task InitializeAsync()
-    {
-        await _container.StartAsync();
-        await WaitForReadyAsync();
-    }
-
+    public Task InitializeAsync() => _container.StartAsync();
     public Task DisposeAsync() => _container.DisposeAsync().AsTask();
-
-    private async Task WaitForReadyAsync()
-    {
-        using var client = CreateClient();
-
-        var timeout = TimeSpan.FromSeconds(2);
-        var start = DateTime.UtcNow;
-
-        while (true)
-        {
-            try
-            {
-                await client.GetVersionAsync();
-                return;
-            }
-            catch (RpcException ex) when (ex.StatusCode is StatusCode.Unavailable or StatusCode.Unknown)
-            {
-                if (DateTime.UtcNow - start > timeout)
-                {
-                    throw;
-                }
-            }
-
-            await Task.Delay(TimeSpan.FromMilliseconds(100));
-        }
-    }
 }
